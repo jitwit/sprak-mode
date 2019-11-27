a21cd81487d8fda396cfaab2b873061d8a9df293
jitwit <jrn@bluefarm.ca>
2019-11-27 03:30:56 -0500

    variable def

--- sprak.el
+++ sprak.el
@@ -1,50 +1,53 @@
 ;; sprak.el --- Using Emacs to interact with Else Heart.Break() and Sprak -*- lexical-binding: t -*-
 
-(defun sprank-program (&optional source)
-  "Yank source or the current buffer to prepare to copy into the game."
+(defun sprank ()
+  "Yank a Sprak program to clipboard. This makes it easy to write
+programs in emacs and paste them into items in the game."
   (interactive)
   (save-excursion
-    (with-current-buffer
-        (or (and source (find-file-existing (concat "sprak/" source ".sprak")))
-            (current-buffer))
-      (kill-ring-save (point-min) (point-max)))))
+    (kill-ring-save (point-min) (point-max))))
 
-(defvar sprak-mode-map
+(defvar sprak-mode-keymap
   (let ((map (make-sparse-keymap)))
-    (define-key map (kbd "C-c c") 'sprank-program)
+    (define-key map (kbd "C-c c") 'sprank)
     map))
 
 (defvar sprak-types
   '("void" "number" "string" "array" "bool" "var"))
-
 (defvar sprak-assignment-operators
   '("=" "+=" "*=" "-=" "/="))
-
 (defvar sprak-operators
   '("+" "-" "/" "*" "<" "<=" "==" ">=" ">" "!="))
+(defvar sprak-control-flow
+  '("loop" "if" "else" "in" "from" "to" "end" "return" "break"))
+(defvar sprak-keyword
+  `(,@sprak-assignment-operators ,@sprak-control-flow))
 
-(defvar sprak-keywords-beginning '("loop" "if"))
-(defvar sprak-keywords-middle '("else" "in" "from" "to"))
-(defvar sprak-keywords-ending '("end" "return" "break"))
-(defvar sprak-keywords
-  `(,@sprak-assignment-operators ;; these are kind of like keywords 
-    ,@sprak-keywords-beginning
-    ,@sprak-keywords-middle
-    ,@sprak-keywords-ending))
-
-(defvar sprak-keywords-face font-lock-builtin-face)
-(defvar sprak-type-face font-lock-type-face)
-(defvar sprak-operator-face font-lock-comment-delimiter-face)
-(defvar sprak-constant-face font-lock-preprocessor-face)
-(defvar sprak-fundef-face font-lock-function-name-face)
-(defvar sprak-funarg-face font-lock-constant-face)
+(defvar sprak-keyword-face
+  font-lock-builtin-face)
+(defvar sprak-type-face
+  font-lock-type-face)
+(defvar sprak-operator-face
+  font-lock-comment-delimiter-face)
+(defvar sprak-constant-face
+  font-lock-preprocessor-face)
+(defvar sprak-fundef-face
+  font-lock-function-name-face)
+(defvar sprak-vardef-face
+  font-lock-function-name-face)
+(defvar sprak-funarg-face
+  font-lock-constant-face)
 
 (defvar sprak-constants-regexp
-  (rx (or "true" "false" (seq (? (any "-+")) (1+ digit) (? "." (0+ digit))))))
+  (rx (or "true"
+          "false"
+          (seq (? (any "-+"))
+               (1+ digit)
+               (? "." (0+ digit))))))
 (defvar sprak-type-regexp
   (regexp-opt sprak-types 'words))
 (defvar sprak-keyword-regexp
-  (regexp-opt sprak-keywords 'symbols))
+  (regexp-opt sprak-keyword 'symbols))
 (defvar sprak-operator-regexp
   (regexp-opt sprak-operators 'symbols))
 
@@ -52,20 +55,29 @@
 ;; would be nice to not highlight commas too.
 (defvar sprak-function-definition-regexp
   (rx (seq (group-n 1 (eval `(or ,@sprak-types)))
-           (1+ space)
+           (+ space)
            (group-n 2 (1+ word))
            (* space)
            "("
-           (group-n 3 (0+ (or word space ",")))
+           (group-n 3 (* (or word space ",")))
            ")")))
 
+(defvar sprak-variable-definition-regexp
+  (rx (seq (group-n 1 (eval `(or ,@sprak-types)))
+           (+ space)
+           (group-n 2 (1+ word))
+           (* space)
+           (group-n 3 "="))))
+
 (defvar sprak-font-lock-defaults
-  `(((,sprak-function-definition-regexp 
-      (1 ,sprak-type-face)
-      (2 ,sprak-fundef-face)
-      (3 ,sprak-funarg-face))
+  `(((,sprak-function-definition-regexp (1 ,sprak-type-face)
+                                        (2 ,sprak-fundef-face)
+                                        (3 ,sprak-funarg-face))
+     (,sprak-variable-definition-regexp (1 ,sprak-type-face)
+                                        (2 ,sprak-vardef-face)
+                                        (3 ,sprak-keyword-face))
      (,sprak-type-regexp . sprak-type-face)
-     (,sprak-keyword-regexp . sprak-keywords-face)
+     (,sprak-keyword-regexp . sprak-keyword-face)
      (,sprak-operator-regexp . sprak-operator-face)
      (,sprak-constants-regexp . sprak-constant-face))))
 
@@ -84,7 +96,7 @@
   "Major mode for Sprak programs"
   :syntax-table sprak-syntax-table
   (setq font-lock-defaults sprak-font-lock-defaults)
-  (use-local-map sprak-mode-map))
+  (use-local-map sprak-mode-keymap))
 
 (add-to-list 'auto-mode-alist '("\\.sprak$" . sprak-mode))
 
